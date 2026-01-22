080a3d2170a63ea56ed59573ef23c5ce6e49a8fa
faizanahmad981 <khawajfaizan258@gmail.com>
2026-01-22 10:57:45 +0500

    images fixesssss

--- app/page.tsx
+++ app/page.tsx
@@ -15,24 +15,12 @@ export default function Home() {
     <main className="min-h-screen bg-[rgb(var(--background))] text-[rgb(var(--foreground))] ">
 
       
-      {/* <div className="text-center space-y-4">
-        <h1 className="text-4xl font-bold text-[rgb(var(--primary))]">
-          My Portfolio
-        </h1>
-
-        <p className="text-lg text-[rgb(var(--secondary))]">
-          Frontend Developer
-        </p>
-
-        <button className="bg-[rgb(var(--primary))] text-white px-6 py-2 rounded-lg">
-          Contact Me
-        </button>
-      </div> */}
+      
       <Navbar/>
       <Banner/>
       <About/>
       <ServicesMainPage/>
-      {/* <Portfolio/> */}
+     
       <PortfolioSection/>
       <TechnicalSkills/>
       <Education/>
